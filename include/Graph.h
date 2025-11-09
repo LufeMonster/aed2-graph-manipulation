50530454469e57d503a3d2ccce791a07f99c52c6
lucasmodolo <lucasmodolo@usp.br>
2025-11-09 19:23:57 -0300

    Adição dos primeiros métodos

--- include/Graph.h
+++ include/Graph.h
@@ -10,10 +10,17 @@ using namespace std;
 
 class Graph {
     public:
-       
+        Graph();
+        ~Graph();
+        int insertNode(string name);
+        void addEdge(int node1, int node2);
+        int deleteNode(string name);
+        void removeEdge(int node1, int node2);
+        int getPosMap(string name);
     private:
         vector<vector<int>> adjacencyMatrix;
         vector<string> cityMappingList;
+        int size;
 };
 
 #endif
--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,2 +1,30 @@
 // Graph.h
 // Class to abstract the Graph manipulation
+
+#include "../include/Graph.h"
+using namespace std;
+
+// ----------------------------------------------------------------
+Graph::Graph() {
+    size = 0;
+}
+
+// ----------------------------------------------------------------
+Graph::~Graph() {
+
+}
+
+// ----------------------------------------------------------------
+int Graph::insertNode(string name) {
+    cityMappingList.push_back(name);
+    return ++size;
+}
+
+// ----------------------------------------------------------------
+int Graph::getPosMap(string name) {
+    for(int i = 0; i < size ; i++) {
+        if (cityMappingList[i] == name) // talvez transformar ambas em lower case?
+            return i;
+    }
+    return -1;
+}
